804b33a79056936fa05d7df8c08669e1ceae3d25
Claude Sortwell <claude.sortwell@gmail.com>
2019-12-26 10:30:12 +1100

    More doc fixes

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const STRIPE_API = require('./config/stripe-functions.js');
 
 // Init App
 const app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
  
 // Passport config
 require('./config/passport')(passport);
